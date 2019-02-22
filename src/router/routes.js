cad85da4b19cf2db562402910ae17583d82b1db4
ssw <1094860113@qq.com>
2019-02-22 20:01:05 +0800

    静态页面搭建完成

---
+++ src/router/routes.js
@@ -0,0 +1,30 @@
+/*
+用来保存所有路由的数组的模块
+ */
+import Msite from '../pages/Msite/Msite.vue'
+import Order from '../pages/Order/Order.vue'
+import Search from '../pages/Search/Search.vue'
+import Profile from '../pages/Profile/Profile.vue'
+
+ export default [
+     {
+       path:'/msite',
+       component:Msite
+     },
+     {
+       path:'/order',
+       component:Order
+     },
+     {
+       path:'/search',
+       component:Search
+     },
+     {
+       path:'/profile',
+       component:Profile
+     },
+     {
+       path:'/',
+       redirect:'/msite'
+     }
+ ]
