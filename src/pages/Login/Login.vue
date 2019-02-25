13a47f34379ecd39b2ea6316a1beb684f571bed0
ssw <1094860113@qq.com>
2019-02-25 13:50:49 +0800

    登录界面两种登录方式切换，手机验证和验证码倒计时完成

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,16 +4,19 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on:loginWay}"  @click="loginWay=true">短信登录</a>
+          <a href="javascript:;" :class="{on:!loginWay}" @click="loginWay=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on:loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification" >获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!isRightPhone" class="get_verification"
+              :class="{right:isRightPhone}" @click.prevent="sendCode">
+                {{countDown > 0 ? `已发送(${countDown}s)` : '获取验证码'}}
+              </button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -23,7 +26,7 @@
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on:!loginWay}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
@@ -54,7 +57,29 @@
 
 <script>
   export default {
-
+    data(){
+      return {
+        loginWay:true,  //true短信登录 false密码登录
+        phone:'',  //手机号
+        countDown:0   //倒计时
+      }
+    },
+    computed:{
+      isRightPhone(){
+        return /^1\d{10}$/.test(this.phone)
+      }
+    },
+    methods:{
+      sendCode(){
+        this.countDown = 30  //倒计时总时长
+        const intervalId = setInterval(() => {
+          this.countDown--
+          if(this.countDown === 0){
+            clearInterval(intervalId)
+          }
+        },1000)
+      }
+    }
   }
 </script>
 
@@ -119,6 +144,8 @@
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right
+                  color black
             .login_verification
               position relative
               margin-top 16px
