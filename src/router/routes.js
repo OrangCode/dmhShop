ad8545af1fcfd81f572363d764003d08e6279500
ssw <1094860113@qq.com>
2019-02-26 11:24:04 +0800

    商家信息静态页面完成

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,10 @@ import Order from '../pages/Order/Order.vue'
 import Search from '../pages/Search/Search.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import Login from '../pages/Login/Login.vue'
+import Shop from '../pages/Shop/Shop.vue'
+import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
+import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
+import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
 
 
  export default [
@@ -44,5 +48,28 @@ import Login from '../pages/Login/Login.vue'
    {
      path:'/login',
      component:Login
-   }
+   },
+   {
+     path:'/shop',
+     component:Shop,
+     children:[
+       {
+         path:'/shop/goods',
+         component:ShopGoods
+       },
+       {
+         path:'/shop/info',
+         component:ShopInfo
+       },
+       {
+         path:'/shop/ratings',
+         component:ShopRatings
+       },
+       {
+         path:'',
+         redirect:'/shop/goods'
+       },
+     ]
+   },
+
  ]
